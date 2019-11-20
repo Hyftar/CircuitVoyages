b36c07d142e7a662ba0d7785e208aad12e26fd50
Hyftar <landry_simon@pm.me>
2019-11-19 21:09:41 -0500

    Add homemade MVC framework

---
+++ Core/View.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Core;
+
+class View
+{
+
+    /**
+     * Render a view file (.php or .html)
+     */
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+
+        $file = dirname(__DIR__) . "/App/Views/$view";  // relative to Core directory
+
+        if (is_readable($file)) {
+            require $file;
+        } else {
+            throw new \Exception("$file not found");
+        }
+    }
+
+    /**
+     * Render a view template using Twig
+     */
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig_Loader_Filesystem(dirname(__DIR__) . '/App/Views');
+            $twig = new \Twig_Environment($loader);
+        }
+
+        echo $twig->render($template, $args);
+    }
+}
