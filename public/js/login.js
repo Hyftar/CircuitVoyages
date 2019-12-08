aa4bf50cc0452f79e6e6418c326e78da9c99a93c
Hyftar <landry_simon@pm.me>
2019-12-08 12:59:23 -0500

    Add scopes to js code

--- public/js/login.js
+++ public/js/login.js
@@ -1,34 +1,33 @@
-let loginContainer = document.getElementById('login-modal')
-loginContainer.onclick = (e) => {
-  for (let element of document.querySelectorAll('#login-modal .card, #login-modal .panel')) {
-    if (element.contains(e.target)) {
-      return
+(() => {
+  let loginContainer = document.getElementById('login-modal')
+  loginContainer.onclick = (e) => {
+    for (let element of document.querySelectorAll('#login-modal .card, #login-modal .panel')) {
+      if (element.contains(e.target)) {
+        return
+      }
     }
+
+    loginContainer.classList.add('hidden')
   }
 
-  loginContainer.classList.add('hidden')
-}
 
-
-let loginLinks = ['nav-login-link', 'drawer-login-link']
-for (let link of loginLinks) {
-  link = document.getElementById(link)
+  link = document.getElementById('nav-login-link')
   link.onclick = () => loginContainer.classList.remove('hidden')
-}
 
 
-let loginModalContainer = document.getElementById('login-modal-container')
-let loginRegisterLink = document.getElementById('login-register-link')
-let loginResetLink = document.getElementById('forgot-password')
-let registerForm = document.getElementById('register-user-form')
-let resetForm = document.getElementById('reset-modal-container')
+  let loginModalContainer = document.getElementById('login-modal-container')
+  let loginRegisterLink = document.getElementById('login-register-link')
+  let loginResetLink = document.getElementById('forgot-password')
+  let registerForm = document.getElementById('register-user-form')
+  let resetForm = document.getElementById('reset-modal-container')
 
-loginRegisterLink.onclick = () => {
-  loginModalContainer.classList.add('hidden')
-  registerForm.classList.remove('hidden')
-}
+  loginRegisterLink.onclick = () => {
+    loginModalContainer.classList.add('hidden')
+    registerForm.classList.remove('hidden')
+  }
 
-loginResetLink.onclick = () => {
-  loginModalContainer.classList.add('hidden')
-  resetForm.classList.remove('hidden')
-}
+  loginResetLink.onclick = () => {
+    loginModalContainer.classList.add('hidden')
+    resetForm.classList.remove('hidden')
+  }
+})()
--- public/js/reset.js
+++ public/js/reset.js
@@ -1,5 +1,9 @@
-let resetLoginLink = document.getElementById('reset-login-link')
-resetLoginLink.onclick = () => {
-  resetForm.classList.add('hidden')
-  loginModalContainer.classList.remove('hidden')
-}
+(() => {
+  let resetForm = document.getElementById('reset-modal-container')
+  let loginModalContainer = document.getElementById('login-modal-container')
+  let resetLoginLink = document.getElementById('reset-login-link')
+  resetLoginLink.onclick = () => {
+    resetForm.classList.add('hidden')
+    loginModalContainer.classList.remove('hidden')
+  }
+})()
