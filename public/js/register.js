76c8228f99d23ae4b4064836b7b53797ae5c966a
Hyftar <landry_simon@pm.me>
2019-12-08 12:59:23 -0500

    Add scopes to js code

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,9 @@
-let registerLoginLink = document.getElementById('register-login-link')
-registerLoginLink.onclick = () => {
-  registerForm.classList.add('hidden')
-  loginModalContainer.classList.remove('hidden')
-}
+(() => {
+  let loginModalContainer = document.getElementById('login-modal-container')
+  let registerForm = document.getElementById('register-user-form')
+  let registerLoginLink = document.getElementById('register-login-link')
+  registerLoginLink.onclick = () => {
+    registerForm.classList.add('hidden')
+    loginModalContainer.classList.remove('hidden')
+  }
+})()
