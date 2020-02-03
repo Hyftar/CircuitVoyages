1f185ee3a98c76daac674b1011cb530a525e14d1
boulette12 <43421096+boulette12@users.noreply.github.com>
2020-02-03 15:03:02 -0500

    add cart + paypal

---
+++ public/js/cart-script.js
@@ -0,0 +1,163 @@
+var shoppingCart = (function () {
+
+  cart = [];
+
+  // Constructor
+  function Item(name, date, price) {
+    this.name = name;
+    this.date = date;
+    this.price = price;
+  }
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+
+
+
+  var obj = {};
+
+  // Add to cart
+  obj.addItemToCart = function (name, date, price) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        var mymodal = $('#modalAlertDanger');
+        mymodal.find('.body').text("L'item a déjà été rajouté au panier");
+        mymodal.modal('show');
+
+        saveCart();
+        return;
+      }
+    }
+    var item = new Item(name, date, price);
+    cart.push(item);
+    var mymodal = $('#modalAlertSuccess');
+    mymodal.find('.body').text('Item rajouté au panier');
+    mymodal.modal('show');
+    saveCart();
+  }
+  // Remove item from cart
+  obj.removeItemFromCart = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart.splice(item, 1);
+      }
+      break;
+    }
+
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function () {
+    cart = [];
+    saveCart();
+  }
+
+  // Count cart
+  obj.totalCount = function () {
+    var totalCount = 0;
+    for (var item in cart) {
+      totalCount += 1;
+    }
+    return totalCount;
+  }
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+
+  // Total cart
+  obj.totalCart = function () {
+    var totalCart = 0;
+    for (var item in cart) {
+      totalCart += cart[item].price;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+  // List cart
+  obj.listCart = function () {
+    var cartCopy = [];
+    for (i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for (p in item) {
+        itemCopy[p] = item[p];
+      }
+      itemCopy.total = 12;
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }
+
+  return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// *****************************************
+
+
+// Add item
+setTimeout(function() {
+  $('.add-to-cart').click(function (event) {
+    event.preventDefault();
+    var date = $(this).data('date');
+    var name = $(this).data('name');
+    var price = $(this).data('price');
+    shoppingCart.addItemToCart(name, date, price);
+    displayCart();
+  });
+}, 1000);
+
+
+
+// Clear items
+$('.clear-cart').click(function () {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for (var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name+ "</td>"
+      + "<td>" + cartArray[i].date + "</td>"
+      + "<td>" + cartArray[i].price + "$</td>"
+      + `"<td><button class='delete-item btn btn-danger' data-name="${cartArray[i].name}">X</button></td>"`
+      + "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function (event) {
+  var name = $(this).data('name');
+  shoppingCart.removeItemFromCartAll(name);
+  displayCart();
+})
+
+displayCart();
+
+
